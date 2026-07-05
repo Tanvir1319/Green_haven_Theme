a4c43e6646235abaca9755285f1d05fd92a96d3c
Tanvir1319 <tanvirju@hotmail.com>
2026-07-05 19:34:20 +0600

    Added WooCommerce Shop Page(But Need To Fix Css)

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -36,49 +36,48 @@ do_action( 'woocommerce_before_main_content' );
  * @hooked woocommerce_product_taxonomy_archive_header - 10
  */
 do_action( 'woocommerce_shop_loop_header' ); ?>
- <h1 class="gh-shop-title">Our Shop</h1><?php
+ <h1 class="gh-shop-title"><?php woocommerce_page_title(); ?></h1>
+   <div class="gh-shop-head-row">
+
+    <div class="gh-shop-result-box">
+        <p class="gh-result-count"><?php woocommerce_result_count(); ?></p>
+    </div>
+
+   <?php woocommerce_catalog_ordering(); ?>
+
+</div>
+
+  
+<?php
 if ( woocommerce_product_loop() ) {
 
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action( 'woocommerce_before_shop_loop' );
+    do_action( 'woocommerce_before_shop_loop' );
+    ?>
 
-	woocommerce_product_loop_start();
+    <div class="gh-product-grid">
 
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
-			the_post();
+        <?php
+        if ( wc_get_loop_prop( 'total' ) ) {
+            while ( have_posts() ) {
+                the_post();
 
-			/**
-			 * Hook: woocommerce_shop_loop.
-			 */
-			do_action( 'woocommerce_shop_loop' );
+                do_action( 'woocommerce_shop_loop' );
 
-			wc_get_template_part( 'content', 'product' );
-		}
-	}
+                wc_get_template_part( 'content', 'product' );
+            }
+        }
+        ?>
 
-	woocommerce_product_loop_end();
+    </div>
 
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
+    <?php
+    do_action( 'woocommerce_after_shop_loop' );
+
 } else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
+    do_action( 'woocommerce_no_products_found' );
 }
+?>   <?php
+
 
 /**
  * Hook: woocommerce_after_main_content.
